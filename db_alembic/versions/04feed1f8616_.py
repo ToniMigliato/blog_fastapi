9c5dca7bb606dc5a0d61cdcfac854907d2af2e2c
ToniMigliato <tonimigliato@gmail.com>
2023-05-31 10:54:41 -0300

    updating requirement.txt

---
+++ db_alembic/versions/04feed1f8616_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 04feed1f8616
+Revises: c3039385636c
+Create Date: 2023-05-31 10:37:19.428949
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '04feed1f8616'
+down_revision = 'c3039385636c'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
